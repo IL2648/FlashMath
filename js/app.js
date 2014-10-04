3496a3f224f89a1800b24273bc4cbfca479c543a
dani5447 <drn5447@rit.edu>
2014-10-04 18:23:55 -0400

    Added browser tab icon, start of stats/help
    - Added an icon for the application to appear in the browser tab
    - Added places for help information and statistics to go

--- js/app.js
+++ js/app.js
@@ -13,6 +13,8 @@ angular.module('myApp', [
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
+  $routeProvider.when('/help', {templateUrl: 'partials/help.html', controller: ''});
+  $routeProvider.when('/stats', {templateUrl: 'partials/stats.html', controller: 'StatsCtrl'});
   $routeProvider.when('/business', {templateUrl: 'partials/business.html', controller: 'BusinessCtrl'});
   $routeProvider.otherwise({redirectTo: '/home'});
 }]);
