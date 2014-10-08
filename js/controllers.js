5395e964f23fc224e24c210aa291747804efeaa9
Zachary Nielsen <zen.gameman@gmail.com>
2014-10-08 13:07:22 -0400

    fixed changed desired in class

--- js/controllers.js
+++ js/controllers.js
@@ -17,11 +17,12 @@ angular.module('myApp.controllers', ['nvd3ChartDirectives'])
         $scope.answer = "";
         $scope.has = "warning";
         $scope.correct = false;
+        $scope.newProblem();
         $scope.alert = "warning";
         $scope.messagePre = "Hi there! ";
         $scope.message = "Please enter your answer at the bottom, the equal sign will turn green when you get the correct answer. ";
-        $scope.newProblem();
-        $scope.initialized = 0;
+        $scope.prevDiff = 1;
+        $scope.initialized = 1;
       }
       $scope.check = function() {
         var dec = 1;
@@ -106,17 +107,16 @@ angular.module('myApp.controllers', ['nvd3ChartDirectives'])
         } else {
           var correct = 0;
         }
-        $http({method: 'POST', url: '../php/problem.php?difficulty='+$scope.maxRating+'&success='+correct+'&argOne='+$scope.numone+'&argTwo='+$scope.numtwo+'&operation='+op+'&correct='+$scope.check()+'&answer='+$scope.answer+'&userId='+User.userId}).
+        $http({method: 'POST', url: '../php/problem.php?difficulty='+$scope.prevDiff+'&success='+correct+'&argOne='+$scope.numone+'&argTwo='+$scope.numtwo+'&operation='+op+'&correct='+$scope.check()+'&answer='+$scope.answer+'&userId='+User.userId}).
           success(function(data, status, headers, config) {
           }).
           error(function(data, status, headers, config) {
         });
         }
       }
-      $scope.newProblem = function() {
+      $scope.answerProblem = function() {
         var ops = [];
         var dec = 1;
-        $scope.reportProblem();
         switch($scope.maxRating){
           case 1:
             dec = 10;
@@ -142,10 +142,12 @@ angular.module('myApp.controllers', ['nvd3ChartDirectives'])
                 $scope.alert = "success";
                 $scope.messagePre = "Nice answer! ";
                 $scope.message = "You were correct, " + $scope.numone + " " + $scope.operation + " " + $scope.numtwo + " = " + $scope.answer;
+                $scope.correct = true;
               } else {
                 $scope.alert = "danger";
                 $scope.messagePre = "Too bad! ";
                 $scope.message = "you were wrong, " + $scope.numone + " " + $scope.operation + " " + $scope.numtwo + " = " + ans + ", not " + $scope.answer;
+                $scope.correct = false;
               }
               break;
             case "-":
@@ -154,10 +156,12 @@ angular.module('myApp.controllers', ['nvd3ChartDirectives'])
                 $scope.alert = "success";
                 $scope.messagePre = "Nice answer! ";
                 $scope.message = "You were correct, " + $scope.numone + " " + $scope.operation + " " + $scope.numtwo + " = " + $scope.answer;
+                $scope.correct = true;
               } else {
                 $scope.alert = "danger";
                 $scope.messagePre = "Too bad! ";
                 $scope.message = "you were wrong, " + $scope.numone + " " + $scope.operation + " " + $scope.numtwo + " = " + ans + ", not " + $scope.answer;
+                $scope.correct = false;
               }
               break;
             case "x":
@@ -166,10 +170,12 @@ angular.module('myApp.controllers', ['nvd3ChartDirectives'])
                 $scope.alert = "success";
                 $scope.messagePre = "Nice answer! ";
                 $scope.message = "You were correct, " + $scope.numone + " " + $scope.operation + " " + $scope.numtwo + " = " + $scope.answer;
+                $scope.correct = true;
               } else {
                 $scope.alert = "danger";
                 $scope.messagePre = "Too bad! ";
-               $scope.message = "you were wrong, " + $scope.numone + " " + $scope.operation + " " + $scope.numtwo + " = " + ans + ", not " + $scope.answer;
+                $scope.message = "you were wrong, " + $scope.numone + " " + $scope.operation + " " + $scope.numtwo + " = " + ans + ", not " + $scope.answer;
+                $scope.correct = false;
               }
               break;
             case "/":
@@ -178,13 +184,99 @@ angular.module('myApp.controllers', ['nvd3ChartDirectives'])
                 $scope.alert = "success";
                 $scope.messagePre = "Nice answer! ";
                 $scope.message = "You were correct, " + $scope.numone + " " + $scope.operation + " " + $scope.numtwo + " = " + $scope.answer;
+                $scope.correct = true;
               } else {
                 $scope.alert = "danger";
                 $scope.messagePre = "Too bad! ";
-                $scope.message = "you were wrong, " + $scope.numone + " " + $scope.operation + " " + $scope.numtwo + " = " + ans + ", not " + $scope.answer;              }
+                $scope.message = "you were wrong, " + $scope.numone + " " + $scope.operation + " " + $scope.numtwo + " = " + ans + ", not " + $scope.answer; 
+                $scope.correct = false;
+              }
               break;
           }
         }
+        $scope.reportProblem();
+        $scope.prevDiff = $scope.maxRating;
+        if($scope.addition){
+          ops += "+";
+        }
+        if($scope.subtraction){
+          ops += "-";
+        }
+        if($scope.multiplication){
+          ops += "x";
+        }
+        if($scope.division){
+          ops += "/";
+        }
+        $scope.operation = ops[Math.floor(Math.random() * ops.length)];
+        switch($scope.maxRating){ //there needs to be a special case for division, cause that shit could be really hard even with small number.... also needs to make sure the second Number is smaller than the first for the first difficulty
+          case 1:
+            if($scope.operation == "-"){
+              $scope.numone = Math.floor(Math.random() * 10) + 1;
+              $scope.numtwo = Math.floor(Math.random() * $scope.numtwo) + 1;
+            } else if($scope.operation == "/"){
+              $scope.numtwo = Math.floor(Math.random() * 10) + 1;
+              $scope.numone = $scope.numtwo * (Math.floor(Math.random() * 5) + 1);
+            } else {
+              $scope.numone = Math.floor(Math.random() * 10) + 1;
+              $scope.numtwo = Math.floor(Math.random() * 10) + 1;
+            }
+            break;
+          case 2:
+            if($scope.operation == "/"){
+              $scope.numtwo = Math.floor(Math.random() * 100) + 1;
+              $scope.numone = $scope.numtwo * (Math.floor(Math.random() * 10) + 1);
+            } else {
+              $scope.numone = Math.floor(Math.random() * 100) + 1;
+              $scope.numtwo = Math.floor(Math.random() * 100) + 1;
+            }
+            break;
+          case 3:
+            if($scope.operation == "/"){
+              $scope.numtwo = Math.floor(Math.random() * 20) + 1;
+              $scope.numone = Math.floor(Math.random() * 20) + 1;
+            } else {
+              $scope.numone = (Math.floor(Math.random() * 100) + 1)/10;
+              $scope.numtwo = (Math.floor(Math.random() * 100) + 1)/10;
+            }
+            break;
+          case 4:
+            $scope.numone = (Math.floor(Math.random() * 1000) + 1)/10;
+            $scope.numtwo = (Math.floor(Math.random() * 1000) + 1)/10;
+            break;
+          case 5:
+            $scope.numone = (Math.floor(Math.random() * 100000) + 1)/1000;
+            $scope.numtwo = (Math.floor(Math.random() * 100000) + 1)/1000;
+            break;
+        }
+        $scope.has = "warning";
+        $scope.correct = false;
+        $scope.answer = "";
+      }
+      $scope.newProblem = function() {
+        var ops = [];
+        var dec = 1;
+        $scope.prevDiff = $scope.maxRating;
+        switch($scope.maxRating){
+          case 1:
+            dec = 10;
+            break;
+          case 2:
+            dec = 100;
+            break;
+          case 3:
+            dec = 100;
+            break;
+          case 4:
+            dec = 1000;
+            break;
+          case 5:
+            dec = 10000;
+            break;
+        }
+        $scope.alert = "warning";
+        $scope.messagePre = "Didn't like that one? ";
+        $scope.message = "Hopefully this problem is more to your liking.";
         if($scope.addition){
           ops += "+";
         }
